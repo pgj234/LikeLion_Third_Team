d5c9becc076c33019ac11ad1d27a18ac3057a3c6
pgj234 <pgj94@naver.com>
2025-06-09 13:34:02 +0900

    기본 프로젝트
    기본 프로젝트

---
+++ Assets/PGJ/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GameManager : SingletonBehaviour<GameManager>
+{
+    int bpm = 0;
+    double currentTime = 0d;
+
+    [SerializeField] Transform noteAppear = null;
+    [SerializeField] GameObject NoteObj = null;
+
+    protected override void Init()
+    {
+        base.Init();
+
+
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= 60d / bpm)
+        {
+            GameObject noteObj = Instantiate(NoteObj, noteAppear.position, Quaternion.identity);
+            currentTime -= 60d / bpm;
+        }
+    }
+}
---
+++ Assets/PGJ/Scripts/SoundManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+// BGM 목록 enum
+public enum BGM
+{
+    Bgm,
+
+}
+
+// SFX 목록 enum
+public enum SFX
+{
+    EscapeComplete,
+
+}
+
+public class SoundManager : SingletonBehaviour<SoundManager>
+{
+    AudioSource bgmAudioSource;
+    AudioSource sfxAudioSource;
+
+    public AudioClip[] bgmClip;
+    public AudioClip[] sfxClip;
+
+
+
+    protected override void Init()
+    {
+        base.Init();
+
+        bgmAudioSource = transform.Find("BGM").GetComponent<AudioSource>();
+        sfxAudioSource = transform.Find("SFX").GetComponent<AudioSource>();
+    }
+
+
+
+    public void PlayBGM(BGM _bgm)
+    {
+        bgmAudioSource.clip = bgmClip[(int)_bgm];
+        bgmAudioSource.Play();
+    }
+
+    public void StopBGM()
+    {
+        bgmAudioSource.Stop();
+    }
+
+
+
+    public void PlaySFX(SFX _sfx)
+    {
+        sfxAudioSource.clip = sfxClip[(int)_sfx];
+        sfxAudioSource.Play();
+    }
+}
