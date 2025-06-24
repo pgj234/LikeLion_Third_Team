66dfc498b2c1d0f785917378872c6bc70f06e8d8
HSangJin <sdit111@gmail.com>
2025-06-24 10:43:37 +0900

    HSJ
    시작씬 완료

--- Assets/100BestEffectPack/Script/csLightIntancityControl.cs
+++ Assets/100BestEffectPack/Script/csLightIntancityControl.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Events;
 
 public class csLightIntancityControl : MonoBehaviour {
 	
+	public enum LightStopAction
+	{
+		None, Disable, Destroy, Callback
+    }
+
 	public Light _light;
 	float _time = 0;
 	public float Delay = 0.5f;
 	public float Down = 1;
+	public LightStopAction stopAction;
+	public UnityAction stopCallback;
 
 	void Update ()
 	{
@@ -18,7 +26,24 @@ public class csLightIntancityControl : MonoBehaviour {
 				_light.intensity -= Time.deltaTime*Down;
 
 			if(_light.intensity <= 0)
-				_light.intensity = 0;
+			{
+                _light.intensity = 0;
+				switch(stopAction)
+				{
+					case LightStopAction.Disable:
+                        gameObject.SetActive(false);
+                        break;
+                    case LightStopAction.Destroy:
+                        Destroy(gameObject);
+                        break;
+                    case LightStopAction.Callback:
+                        stopCallback?.Invoke();
+                        break;
+                    default:
+                        break;
+                }
+            }
+				
 		}
 	}
 }
