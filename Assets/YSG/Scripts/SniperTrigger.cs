26b12f4f6329887d5598b8ca70b23f9aa20c44d9
yang491658 <park49164357@gmail.com>
2025-06-29 03:17:49 +0900

    작업 중

--- Assets/YSG/Scripts/SniperTrigger.cs
+++ Assets/YSG/Scripts/SniperTrigger.cs
@@ -9,9 +9,30 @@ public class SniperTrigger : MonoBehaviour
         sniper = GetComponentInParent<Sniper>();
     }
 
+    #region 애니메이션
     private void ShootOver() => sniper.ShootOverEvent();
     private void ReloadOver() => sniper.ReloadOverEvent();
+    #endregion
 
-
+    #region 사운드
     private void ShootSound() => SoundManager.Instance.PlaySFX(SFX.SniperShoot);
+    private void ReloadSound(int num)
+    {
+        switch (num)
+        {
+            case 1:
+                SoundManager.Instance.PlaySFX(SFX.SniperReload1);
+                break;
+            case 2:
+                SoundManager.Instance.PlaySFX(SFX.SniperReload2);
+                break;
+            case 3:
+                SoundManager.Instance.PlaySFX(SFX.SniperReload3);
+                break;
+            case 4:
+                SoundManager.Instance.PlaySFX(SFX.SniperReload4);
+                break;
+        }
+    }
+    #endregion
 }
