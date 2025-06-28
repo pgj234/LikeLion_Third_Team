0aec8a1c7eb765f6cededadce9e5f9adecbdf2cd
pgj234 <pgj94@naver.com>
2025-06-29 00:23:09 +0900

    PGJ
    보스 오브젝트 추가

---
+++ Assets/PGJ/Scripts/BossHP.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BossHP : MonoBehaviour
+{
+    Transform camera;
+
+    void Awake()
+    {
+        camera = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        transform.LookAt(camera);
+    }
+}
